2e5ae7d776b35ef38f2982b099c2e6fbb0342f38
Shan10 <43813132+ShanTen@users.noreply.github.com>
2022-05-14 00:48:05 +0530

    Updated Index.js

--- index.js
+++ index.js
@@ -1,20 +1,55 @@
 const express = require("express");
-const framesArr = require("./frames");
+const framesArr = require("./arr");
 const app = express();
+const colors = require('colors/safe');
 var port = process.env.PORT || 3030;
+var clsStr = (" ".repeat(40)+"\n").repeat(40)
 
-var clsStr = (" ".repeat(40)+"\n").repeat(30)
+//------------------EDIT-able-----------------------------------------------
+const wantColor = false;
+const interval = 60;
+
+const colorsOptions = [
+    'red',
+    'yellow',
+    'green',
+    'blue',
+    'magenta',
+    'cyan',
+    'white'
+];
+//---------------------------------------------------------------------------
+
+const numColors = colorsOptions.length;
+const selectColor = previousColor => {
+let color;
+
+do {
+    color = Math.floor(Math.random() * numColors);
+} while (color === previousColor);
+return color;
+};
 
 app.get('/',(req,res)=>{
+
+    let lastColor;
     res.setHeader('Content-Type', 'text/html; charset=utf-8');
     res.setHeader('Transfer-Encoding', 'chunked');
 
     count = 0
 
     setInterval(function(){
-        res.write(`${framesArr[count]}`)
+        
+        if(wantColor){
+            const newColor = lastColor = selectColor(lastColor);
+            res.write(`${colors[colorsOptions[newColor]](framesArr[count])}`)
+        }
+        else{
+            res.write(`${framesArr[count]}`)
+        }
+        res.write("\033[0;0H");
         count++
-        setTimeout(() => {
+        setTimeout(() => { //Drawing of frames
             res.write("\033[0;0H"); //Ansii to reset cursor
             res.write(clsStr)
             res.write("\033[0;0H");
@@ -24,7 +59,7 @@ app.get('/',(req,res)=>{
             count=0
         }
 
-    },200)
+    },interval)
 
 });
 
